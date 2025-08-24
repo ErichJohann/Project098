4db3b797290648e7e2336e73468245f7068bd2ca
ErichJohann <97558441+ErichJohann@users.noreply.github.com>
2025-08-23 22:55:42 -0300

    Load photo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
 def camera():
     #abre camera
@@ -8,33 +10,73 @@ def camera():
         print('falha ao abrir camera')
         exit(1)
 
-    imagem = None
+    img = None
     while True:
         rt, frame = cam.read()
         if not rt:
             print('erro ao capturar frame')
+            break
+
+        cv2.imshow('Pressione \'c\' para captura o frame ou \'q\' para quitar' ,frame)
 
         key = cv2.waitKey(1) & 0xFF
-        if key == ord('c'):
-            imagem = frame.copy()
-            break
         if key == ord('q'):
+            img = None
             break
-
-        cv2.imshow('Pressione \'c\' para captura o frame ou \'q\' para sair' ,frame)
+        if key == ord('c'):
+            img = frame.copy()
+            break   
 
     cam.release()
     cv2.destroyAllWindows()
-    return imagem
+    return img
 
-def main():
-    key = ord('q')
-    while key == ord('q'):
+
+def takePhoto():
+    img = None
+    while True:
         img = camera()
         if img is not None:
-            cv2.imshow('Pressione \'q\' para tirar outra foto ou \'f\' para prosseguir', img)
-            key = cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            cv2.imshow("Pressione \'t\' para tirar outra foto ou \'f\' para prosseguir", img)
+            key = cv2.waitKey(0) & 0xFF
+            if key == ord('f'):
+                cv2.destroyAllWindows()
+                return 'f', img
+        else:
+            return None, ''
+        cv2.destroyAllWindows()
+
+
+def uploadPhoto():
+    root = Tk()
+    root.withdraw()
+    root.attributes('-topmost', True)
+    imgPath = askopenfilename(title='Selecione uma imagem', filetypes=[("Arquivos de image", "*.jpg *.jpeg *.png")])
+    if imgPath:
+        img = cv2.imread(imgPath)
+        mode = 'f'
+    else:
+        img = None
+        mode = ''
+    root.destroy()
+    return img, mode
+
 
 if __name__ == "__main__":
-    main()
+    mode = input("\'t\'- Tirar foto\n\'u\'- Upload foto\n").lower()
+    while True:   
+        match mode:
+            case 't':
+                img, mode = takePhoto()
+
+            case 'u':
+                img, mode = uploadPhoto()
+
+            case _:
+                mode = input("\'t\' - Tirar foto\n\'u\' - Upload foto\n").lower()
+        if img is not None:
+            print("imagem salva")
+            break
+
+
+    print("imagem enviada")
