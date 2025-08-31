d28f8edda1254df64e0a6c5ab80aee20fabf4834
ErichJohann <97558441+ErichJohann@users.noreply.github.com>
2025-08-31 19:11:05 -0300

    tesseract test

--- ocr_benchmark/benchmark.py
+++ ocr_benchmark/benchmark.py
@@ -1,10 +1,24 @@
 import pytesseract
 import cv2
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 def processData(path):
     #opens image on grayscale
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    binImg = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 35, 11) #thereshhold + binary
+    img = cv2.imread(r'imgs/invi.png', cv2.IMREAD_GRAYSCALE)
+    binImg = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 11) #thereshhold + binary
     denoisedImg = cv2.medianBlur(binImg, 3) #blur for noise removal
-
+    cv2.imshow('a', denoisedImg)
+    cv2.waitKey(0)
     return denoisedImg
+    #return cv2.imread(r'imgs/internet.png')
+
+def tes(img):
+    confg = r'--oem 3 --psm 6'  # engine LSTM + modo "parágrafos"
+    tesPredic = pytesseract.image_to_string(img, config=confg, lang='por')
+    print(tesPredic)
+
+def easy(img):
+    return img
+
+oi = processData('a')
+tes(oi)
