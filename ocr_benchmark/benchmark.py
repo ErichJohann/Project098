ed1845a447f5bb41a8303904dfe423b9faf43fed
ErichJohann <97558441+ErichJohann@users.noreply.github.com>
2025-08-31 21:22:31 -0300

    easyOCR test

--- ocr_benchmark/benchmark.py
+++ ocr_benchmark/benchmark.py
@@ -1,24 +1,28 @@
 import pytesseract
+import easyocr
 import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 def processData(path):
     #opens image on grayscale
-    img = cv2.imread(r'imgs/invi.png', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(r'imgs/chaff.png', cv2.IMREAD_GRAYSCALE)
     binImg = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 11) #thereshhold + binary
     denoisedImg = cv2.medianBlur(binImg, 3) #blur for noise removal
-    cv2.imshow('a', denoisedImg)
-    cv2.waitKey(0)
-    return denoisedImg
-    #return cv2.imread(r'imgs/internet.png')
+    #cv2.imshow('a', denoisedImg)
+    #cv2.waitKey(0)
+    #return denoisedImg
+    return cv2.imread(r'imgs/internet.png')
 
 def tes(img):
     confg = r'--oem 3 --psm 6'  # engine LSTM + modo "parágrafos"
-    tesPredic = pytesseract.image_to_string(img, config=confg, lang='por')
-    print(tesPredic)
+    tesPredict = pytesseract.image_to_string(img, config=confg, lang='por')
+    print(tesPredict)
 
 def easy(img):
-    return img
+    reader = easyocr.Reader(['pt'])
+    predict = reader.readtext(img, detail=0, paragraph=True, decoder='beamsearch')
+    for txt in predict:
+        print(txt + ' ')
 
 oi = processData('a')
-tes(oi)
+easy(oi)
